0cf1ef9b975e74a07db123ac9642c8a744d81fb1
FrankApiyo <franklineapiyo@gmail.com>
2019-04-14 18:19:29 +0300

    added rss feeds from cnn and fox news

--- headlines.py
+++ headlines.py
@@ -5,21 +5,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"; 
+BBC_FEED = {'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
+			'cnn': "http://rss.cnn.com/rss/edition.rss",
+			'fox': "http://feeds.foxnews.com/foxnews/latest",
+			'iol': 'http://www.iol.co.za/cmlink/1.640'}; 
 
 @app.route("/")
-def get_news():
-	feed = feedparser.parse(BBC_FEED)
+@app.route("/bbc")
+def bbc():
+	return get_news("bbc")
+
+@app.route("/cnn")
+def cnn():
+	return get_news("cnn")
+
+def get_news(publication):
+	feed = feedparser.parse(BBC_FEED[publication])
 	first_article = feed['entries'][0]
 	return """<html>
 		<body>
-			<h1>BBC headlines</h1>
+			<h1>{3} headlines</h1>
 			<b>{0}</b><br/>
 			<i>{1}</i><br/>
 			<p>{2}</p><br/>
 		</body>
 	</html>
-	""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+	""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"), publication)
 
 if __name__ == "__main__":
 	app.run(port=5000, debug=True)
